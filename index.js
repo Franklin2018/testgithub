b32f6210449be6aff0b970ccf258e9a15f7e85b2
Franklin <franklin.ojc@gmail.com>
2021-07-07 11:59:59 -0400

    commit resolviendo conflictos a  la rama main

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 // "helo word"
-<<<<<<< HEAD
-<<<<<<< HEAD
 // "hello luis"
-=======
 //"hola frankin"
->>>>>>> e84c70c3289b0443d188223f0be126b965e69ec1
-=======
-//"hola frankin"
->>>>>>> e84c70c3289b0443d188223f0be126b965e69ec1
